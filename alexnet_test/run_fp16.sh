65c6c65c23f8f339f6943e885217d7db3e71ab7c
mir-of <mir-of@outlook.com>
2019-08-20 16:00:44 +0800

    add scripts for run alexnet fp32 & fp16

---
+++ alexnet_test/run_fp16.sh
@@ -0,0 +1,19 @@
+python ../../benchmarks/scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py \
+--use_fp16=True \
+--model=alexnet \
+--train_dir=./train_outputs \
+--input_preprocessor=official_models_imagenet \
+--data_dir=/home/qiaojing/tmp/dataset/tf_PNG1_227 \
+--data_name=imagenet \
+--num_gpus=1 \
+--datasets_use_prefetch=False \
+--datasets_num_private_threads=1 \
+--batch_size=1 \
+--num_batches=100 \
+--display_every=1 \
+--save_model_steps=1 \
+--num_warmup_batches=0 \
+--init_learning_rate=0.00001 \
+--loss_type_to_report='base_loss' \
+--optimizer='sgd' \
+--weight_decay=0
--- alexnet_test/set_tf_env.sh
+++
@@ -1,4 +0,0 @@
-unset PYTHONPATH
-source ~/virtualenvs/facenet/bin/activate
-export PYTHONPATH=$PYTHONPATH:../../benchmarks
-export PYTHONPATH=$PYTHONPATH:../../models
